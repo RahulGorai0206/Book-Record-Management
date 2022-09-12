7a479ef7188b28ca7225a8e07ab7abf0c287a49b
RahulGorai0206 <rahulgorai0206@gmail.com>
2022-09-12 19:34:45 +0530

    Initial setup to go

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const app =express();
+const port=8080; // http://localhost:8080/
+
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+    res.status(200).send({
+        Message:"Server is up"
+    });
+});
+app.all("*",(req,res)=>{
+    res.status(404).send({
+        Message:"Invalid URL"
+    });
+})
+
+app.listen(port,()=>{
+    console.log(`Server started in port: ${port}`);
+});
